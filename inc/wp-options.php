3f2ea9b68f5a3a265574643ce4d6310bd591b7be
alimir <alimir@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-15 15:24:52 +0000

    Upgraded to 1.5
    git-svn-id: https://plugins.svn.wordpress.org/wp-ulike/tags/1.5@1007969 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ inc/wp-options.php
@@ -0,0 +1,171 @@
+<?php
+
+function wp_ulike_adminMenu() {
+	add_options_page(__( 'WP Ulike', 'alimir' ), __( 'WP Ulike', 'alimir' ), 'administrator', __FILE__, 'wp_ulike_settings_page', __FILE__);
+	add_action('admin_init', 'wp_ulike_register_mysettings');
+}
+add_action('admin_menu', 'wp_ulike_adminMenu');
+
+function wp_ulike_register_mysettings() { // whitelist options
+	register_setting( 'wp_ulike_options', 'wp_ulike_onPage' );
+	register_setting( 'wp_ulike_options', 'wp_ulike_textOrImage' );
+	register_setting( 'wp_ulike_options', 'wp_ulike_text' );
+	register_setting( 'wp_ulike_options', 'wp_ulike_btn_text' );
+	register_setting( 'wp_ulike_options', 'wp_ulike_dislike_text' );
+	register_setting( 'wp_ulike_options', 'wp_ulike_onlyRegistered' );
+	register_setting( 'wp_ulike_options', 'wp_ulike_user_like_box' );
+	register_setting( 'wp_ulike_options', 'wp_ulike_format_number' );
+	register_setting( 'wp_ulike_options', 'wp_ulike_style' );
+	register_setting( 'wp_ulike_options', 'wp_ulike_btn_bg' );
+	register_setting( 'wp_ulike_options', 'wp_ulike_btn_border' );
+	register_setting( 'wp_ulike_options', 'wp_ulike_btn_color' );
+	register_setting( 'wp_ulike_options', 'wp_ulike_counter_bg' );
+	register_setting( 'wp_ulike_options', 'wp_ulike_counter_border' );
+	register_setting( 'wp_ulike_options', 'wp_ulike_counter_color' );	
+}
+
+
+function wp_ulike_settings_page() {
+?>
+<div class="wrap">
+			
+		<h3><?php _e('Configuration','alimir'); ?></h3>
+			<form method="post" action="options.php">
+			<?php settings_fields('wp_ulike_options'); ?>
+			<?php do_settings_sections( 'wp_ulike_options' ); ?>
+			<table class="form-table">
+				<tr>
+					<th scope="row"><?php _e('Image or text?', 'alimir'); ?></th>
+					<td>
+						<label>
+						<input name="wp_ulike_textOrImage" type="radio" value="image" <?php checked('image', get_option( 'wp_ulike_textOrImage' ) ); ?> /><img src="<?php echo plugins_url('assets/css/add.png', dirname(__FILE__)); ?>" alt="image">
+						</label>
+						<br />
+						<label>
+						<input name="wp_ulike_textOrImage" type="radio" value="text" <?php checked('text', get_option( 'wp_ulike_textOrImage' ) ); ?> />
+						</label>
+						<label>
+						<input type="text" name="wp_ulike_text" id="wp_ulike_text" value="<?php echo get_option('wp_ulike_text'); ?>" />
+						</label>
+					</td>
+				</tr>
+				<tr>
+					<th scope="row"><?php _e('Like Text', 'alimir'); ?></th>
+					<td>
+						<label for="wp_ulike_btn_text">
+						<input type="text" name="wp_ulike_btn_text" id="wp_ulike_btn_text" value="<?php echo get_option('wp_ulike_btn_text'); ?>" />
+						</label>
+					</td>
+				</tr>
+				<tr>
+					<th scope="row"><?php _e('Dislike Text?', 'alimir'); ?></th>
+					<td>
+						<label for="wp_ulike_dislike_text">
+						<input type="text" name="wp_ulike_dislike_text" id="wp_ulike_dislike_text" value="<?php echo get_option('wp_ulike_dislike_text'); ?>" />
+						</label>
+					</td>
+				</tr>
+				<tr>
+					<th scope="row"><?php _e('Automatic display', 'alimir'); ?></th>
+					<td>
+						<label for="wp_ulike_onPage">
+						<input name="wp_ulike_onPage" id="wp_ulike_onPage" type="checkbox" value="1" <?php checked( '1', get_option( 'wp_ulike_onPage' ) ); ?> />
+						<?php _e('<strong>On all posts</strong> (home, archives, search) at the bottom of the post', 'alimir'); ?>
+						</label>
+						<p class="description"><?php _e('If you disable this option, you have to put manually the code', 'alimir'); ?><code dir="ltr">&lt;?php if(function_exists('wp_ulike')) wp_ulike('get'); ?&gt;</code> <?php _e('wherever you want in your template.', 'alimir'); ?></p>
+					</td>
+				</tr>		
+				<tr>
+					<th scope="row"><?php _e('Only registered Users', 'alimir'); ?></th>
+					<td>
+						<label for="wp_ulike_onlyRegistered">			
+						<input name="wp_ulike_onlyRegistered" id="wp_ulike_onlyRegistered" type="checkbox" value="1" <?php checked( '1', get_option( 'wp_ulike_onlyRegistered' ) ); ?> />
+						<?php _e('<strong>Active</strong> this option.', 'alimir'); ?>
+						</label>
+						<p class="description"><?php _e('<strong>Only</strong> registered users have permission to like posts.', 'alimir'); ?></p>
+					</td>
+				</tr>		
+				<tr>
+					<th scope="row"><?php _e('Show Users Like Box', 'alimir'); ?></th>
+					<td>
+						<label for="wp_ulike_user_like_box">
+						<input name="wp_ulike_user_like_box" id="wp_ulike_user_like_box" type="checkbox" value="1" <?php checked( '1', get_option( 'wp_ulike_user_like_box' ) ); ?> />
+						<?php _e('Activate', 'alimir'); ?>
+						</label>
+						<p class="description"><?php _e('Active this option to show users avatar in like box.', 'alimir'); ?></p>
+					</td>
+				</tr>
+				<tr>
+					<th scope="row"><?php _e('Format Number', 'alimir'); ?></th>
+					<td>
+						<label for="wp_ulike_format_number">
+						<input name="wp_ulike_format_number" id="wp_ulike_format_number" type="checkbox" value="1" <?php checked( '1', get_option( 'wp_ulike_format_number' ) ); ?> />
+						<?php _e('Activate', 'alimir'); ?>
+						</label>
+						<p class="description"><?php _e('Convert numbers of Likes with string (kilobyte) format.', 'alimir'); ?><strong> (if -> likes>=1000)</strong></p>
+					</td>
+				</tr>				
+				<tr>
+					<th scope="row"><?php _e('Custom Style', 'alimir'); ?></th>
+					<td>
+						<label for="wp_ulike_style">
+						<input name="wp_ulike_style" id="wp_ulike_style" type="checkbox" value="1" <?php checked( '1', get_option( 'wp_ulike_style' ) ); ?> />
+						<?php _e('Activate', 'alimir'); ?>
+						</label>
+						<p class="description"><?php _e('Active this option to see custom color settings.', 'alimir'); ?></p>
+					</td>
+				</tr>
+				<tr class="checktoshow">
+					<th scope="row"><?php _e('Button style', 'alimir'); ?></th>
+					<td>
+						<label for="wp_ulike_btn_bg">
+						<input type="text" class="my-color-field" id="wp_ulike_btn_bg" name="wp_ulike_btn_bg" value="<?php echo get_option('wp_ulike_btn_bg'); ?>" />
+						</label>
+						<p class="description"><?php _e('Background', 'alimir'); ?></p><br />
+						<label for="wp_ulike_btn_border">
+						<input type="text" class="my-color-field" id="wp_ulike_btn_border" name="wp_ulike_btn_border" value="<?php echo get_option('wp_ulike_btn_border'); ?>" />
+						</label>
+						<p class="description"><?php _e('Border Color', 'alimir'); ?></p><br />
+						<label for="wp_ulike_btn_color">
+						<input type="text" class="my-color-field" id="wp_ulike_btn_color" name="wp_ulike_btn_color" value="<?php echo get_option('wp_ulike_btn_color'); ?>" />
+						</label>
+						<p class="description"><?php _e('Text Color', 'alimir'); ?></p>
+					</td>
+				</tr>	
+				<tr class="checktoshow">
+					<th scope="row"><?php _e('Counter Style', 'alimir'); ?></th>
+					<td>
+						<label for="wp_ulike_counter_bg">
+						<input type="text" class="my-color-field" id="wp_ulike_counter_bg" name="wp_ulike_counter_bg" value="<?php echo get_option('wp_ulike_counter_bg'); ?>" />
+						</label>
+						<p class="description"><?php _e('Background', 'alimir'); ?></p><br />
+						<label for="wp_ulike_counter_border">
+						<input type="text" class="my-color-field" id="wp_ulike_counter_border" name="wp_ulike_counter_border" value="<?php echo get_option('wp_ulike_counter_border'); ?>" />
+						</label>
+						<p class="description"><?php _e('Border Color', 'alimir'); ?></p><br />
+						<label for="wp_ulike_counter_color">
+						<input type="text" class="my-color-field" id="wp_ulike_counter_color" name="wp_ulike_counter_color" value="<?php echo get_option('wp_ulike_counter_color'); ?>" />
+						</label>
+						<p class="description"><?php _e('Text Color', 'alimir'); ?></p>
+					</td>
+				</tr>
+			</table>
+			<?php submit_button(); ?>
+			</form>
+
+	
+	<div id="poststuff" class="ui-sortable meta-box-sortables">
+		<div class="postbox">
+		<h3><?php _e('Like this plugin?','alimir'); ?></h3>
+			<div class="inside">
+				<p>
+				<?php _e('Show your support by Rating 5 Star in <a href="http://wordpress.org/plugins/wp-ulike"> Plugin Directory reviews</a>','alimir'); ?><br />
+				<?php _e('Follow me on <a href="https://www.facebook.com/alimir.ir"> Facebook</a>','alimir'); ?><br />
+				<?php _e('Plugin Author Blog: <a href="http://alimir.ir"> Wordpress & Programming World.</a>','alimir'); ?>
+				</p>
+			</div>
+		</div>
+	</div>
+</div>
+<?php
+}
---
+++ readme.txt
@@ -0,0 +1,97 @@
+=== WP ULike ===
+Contributors: alimir
+Donate link: http://alimir.ir
+Author: Ali Mirzaei
+Tags: wp ulike, wordpress youlike plugin, like button, rating, vote, voting, most liked posts, wordpress like page, wordpress like post, wordpress vote page, wordpress vote post, wp like page, wp like post, wp like plugin
+Requires at least: 3.0
+Tested up to: 4.0
+Stable tag: 1.5
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+WP ULike plugin allows to integrate Ajax Like Button into your WordPress website to allow your visitors to like and dislike pages and posts.
+
+== Description ==
+
+WP ULike plugin allows to integrate a beautiful Ajax Like Button into your wordPress website to allow your visitors to like and dislike pages and posts. Its very simple to use and support a widget to display the most liked posts. 
+
+= Demo =
+
+<a target="_blank" href="http://preview.alimir.ir/wp-ulike-plugin">Click Here</a>
+
+= Features =
+
+*   Clean Design.
+*   Ajax feature to update the data without reloading.
+*   Visitors do not have to register or log in to use the Like Button.
+*   Compatible with WP version 3.0 & above.
+*   Added automatically (no Code required).
+*   Shortcode support.
+*   Simple user like box with avatar support.
+*   Custom Like-Dislike Texts.
+*   Simple custom style with color picker settings.
+*   Widget to show 'Most Liked Posts' And 'Most Liked Users' avatars.
+*   Simple configuration panel.
+*   Support RTL & language file.
+*   And so on...
+
+= How To Use? =
+Just install the plugin and use this function:
+`<?php if(function_exists('wp_ulike')) wp_ulike('get'); ?>`
+<strong>OR</strong> this shortcode:
+`[wp_ulike]`
+<strong>OR</strong> activate the "automatic display" in plugin configuration panel.
+
+= Translations =
+*   English
+*   Persian
+*   France
+*   Chinese (Thanks Changmeng Hu)
+*   Chinese Tradition (Thanks Arefly)
+
+= Plugin Author =
+Website: <a href="http://about.alimir.ir" target="_blank">Ali Mirzaei</a><br />
+Follow on <a href="https://www.facebook.com/alimir.ir" target="_blank">Facebook</a>
+
+== Installation ==
+
+1. Upload plugin folder to the `/wp-content/plugins/` directory.
+2. Activate the plugin through the `Plugins` menu in WordPress.
+
+== Screenshots ==
+
+Screenshots are available in <a href="http://preview.alimir.ir/wp-ulike-plugin" target="_blank">here</a>
+
+== Changelog ==
+
+= 1.5 =
+* Added: Number format option to convert numbers of Likes with string (kilobyte) format.
+* Updated: Persian language.
+
+= 1.4 =
+* Added: Shortcode support.
+
+= 1.3 =
+* Added: Custom style with color picker setting. (for button and counter box)
+* Added: Chinese Tradition (zh_TW) language. (Thanks to Arefly)
+* Updated: Persian language.
+
+= 1.2 =
+* Added: most liked users widget.
+* Added: Chinese (ZH_CN) language. (Thanks to Changmeng Hu) 
+
+= 1.1 =
+* Added: loading spinner.
+* Added: new database table.
+* Added: user dislike support.
+* Added: Simple "user avatar box" at the bottom of every post.
+* Fixes: plugin security and authentication.
+* Updated: language files.
+
+= 1.0 =
+* The initial version
+
+== Upgrade Notice ==
+
+= 1.0 =
+The initial version
